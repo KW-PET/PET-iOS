3f6ec6ce6aff3e3b4dd31af642ff41419cde13c9
ssu00 <kim.jisu@mintech.kr>
2022-10-01 15:37:59 +0900

    Initial Commit

---
+++ pet-client/pet_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  pet_clientApp.swift
+//  pet-client
+//
+//  Created by 김지수 on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct pet_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
