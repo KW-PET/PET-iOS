3063f95a9f6f0887088e544dc4c554697cb75807
ssu00 <kim.jisu@mintech.kr>
2022-10-10 13:47:48 +0900

    feat: 유저 토큰 얻기, 유저 정보 얻기, 연결 끊기 함수 추가

--- pet-client/View/View.swift
+++ pet-client/View/View.swift
@@ -1,8 +1,68 @@
-//
-//  View.swift
-//  pet-client
-//
-//  Created by 김지수 on 2022/10/01.
-//
+import SwiftUI
+import KakaoSDKAuth
+import KakaoSDKUser
 
-import Foundation
+func getKakaoAgreement(){ //회원가입 시 카카오톡으로 넘어가서 동의받는 부분
+    if (UserApi.isKakaoTalkLoginAvailable()) {
+        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+            print("installed \(oauthToken?.accessToken)")
+            print("installed \(error)")
+        }
+    }else{
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            print("non-installed \(oauthToken?.accessToken)")
+            print("non-installed \(error)")
+        }
+    }
+}
+
+func getUserToken(){ //유저 토큰 얻어오기
+    UserApi.shared.accessTokenInfo {(accessTokenInfo, error) in
+        if let error = error {
+            print(error)
+        }
+        else {
+            print("accessTokenInfo() success: \(accessTokenInfo)")
+        }
+    }
+}
+
+func getUserInfo(){ //유저 정보 가져오기
+    UserApi.shared.me() {(user, error) in
+        if let error = error {
+            print(error)
+        }
+        else {
+            print("me() success: \(user)")
+        }
+    }
+}
+
+func disconnectWithKakao(){ //앱과 카카오계정 연결 끊기. 개발 테스트할 때, 혹은 탈퇴 시 사용
+    UserApi.shared.unlink {(error) in
+        if let error = error {
+            print(error)
+        }
+        else {
+            print("unlink() success.")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button(action : getKakaoAgreement){
+            Image("KakaoLogin")
+        }
+        Button(action : getUserToken){
+            Text("유저 토큰")
+        }
+        Button(action : disconnectWithKakao){
+            Text("연결 끊기")
+        }
+        Button(action : getUserInfo){
+            Text("유저 정보")
+        }
+    }
+}
+ 
