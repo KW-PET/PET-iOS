d5292d12713e145ce6e338af4c581b6af8bbec47
ssu00 <rachel4535@naver.com>
2022-10-16 13:44:37 +0900

    chore: 색상 팔레트 추가

---
+++ pet-client/Resources/Palette.swift
@@ -0,0 +1,13 @@
+//
+//  Palette.swift
+//  pet-client
+//
+//  Created by 김지수 on 2022/10/16.
+//
+
+import SwiftUI
+
+struct ColorManager {
+    static let OrangeColor = Color("OrangeColor")
+    static let GreyColor = Color("GreyColor")
+}
